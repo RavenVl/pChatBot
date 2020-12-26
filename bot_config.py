a0fc26ac75dc394ebbcbf5a7b2b65151eeff8ec2
Sergei Gushin <ravenvl71@gmail.com>
2020-12-26 13:53:56 +0300

    add machine learning predict

--- bot_config.py
+++ bot_config.py
@@ -1,5 +1,6 @@
 import json
 
+
 def get_bot_config():
     with open('big_bot_config.json') as f:
         BOT_CONFIG = json.load(f)
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import re, random
 import nltk
 from bot_config import get_bot_config
-BOT_CONFIG = get_bot_config()
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
+BOT_CONFIG = get_bot_config()
+x_input = []
+y = []
+for intent, data in BOT_CONFIG['intents'].items():
+    for el in data['examples']:
+        x_input.append(el)
+        y.append(intent)
+tfidf_vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(2, 4))
+X = tfidf_vectorizer.fit_transform(x_input)
+lin_svc = LinearSVC(penalty='l2')
+lin_svc.fit(X, y)
 
 def filter(text: str):
     text = text.lower()
@@ -24,6 +37,11 @@ def get_intent(text):
             if match(text, ask):
                 return intent
 
+def get_intent_predictive_model(text):
+
+    return lin_svc.predict(tfidf_vectorizer.transform([text]))[0]
+
+
 
 def get_answer_by_intent(intent):
     return random.choice(BOT_CONFIG['intents'][intent]['responses'])
@@ -32,21 +50,23 @@ def get_answer_by_intent(intent):
 def bot(text):
     # понять намерение
     intent = get_intent(text)
+
+    if not intent:
+        intent = get_intent_predictive_model(text)
+
     if intent:
+        print(intent)
         return get_answer_by_intent(intent)
-    # сгененерить ответ по контесксту
-    # TODO mashin learning
 
     # вернуть случайную фразу
     return random.choice(BOT_CONFIG['failure_phrases'])
 
 
 if __name__ == '__main__':
-    # print(bot('прощяй!'))
-    # print(bot('ыфвфывфв'))
     req = ''
     while True:
         req = input('--')
         if req in ['выход', 'достал']:
             break
         print(bot(req))
+    # use_mo()
