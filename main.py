6af81cfd663db0b0dda78f45577c4667e4c1cfeb
Sergei Gushin <ravenvl71@gmail.com>
2020-12-26 10:28:43 +0300

    add bot rev 1

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ BOT_CONFIG = {
         'bay': {
             'examples': ['пока', 'досвидания', 'прощай'],
             'response': ['счастливо', 'если чего возвращайся', 'bay, bay', 'good bay']
+        },
+        'whatsup': {
+            'examples': ['как дела', 'чо как', 'как ты'],
+            'response': ['норм', 'все ок', 'пока живой', 'все отлично']
         }
     },
     'failure_phrases': ['Ничего не понятно', 'Чего, чего?']
@@ -36,8 +40,29 @@ def get_intent(text):
             if match(text, ask):
                 return intent
 
+
 def get_answer_by_intent(intent):
     return random.choice(BOT_CONFIG['intents'][intent]['response'])
 
+
+def bot(text):
+    # понять намерение
+    intent = get_intent(text)
+    if intent:
+        return get_answer_by_intent(intent)
+    # сгененерить ответ по контесксту
+    # TODO mashin learning
+
+    # вернуть случайную фразу
+    return random.choice(BOT_CONFIG['failure_phrases'])
+
+
 if __name__ == '__main__':
-    print(get_answer_by_intent('bay'))
+    # print(bot('прощяй!'))
+    # print(bot('ыфвфывфв'))
+    req = ''
+    while True:
+        req = input('--')
+        if req in ['выход', 'достал']:
+            break
+        print(bot(req))
